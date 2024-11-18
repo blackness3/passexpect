76950bd10afcf123ae59e07b3c057991a68a14b4
blackness3 <57695239+blackness3@users.noreply.github.com>
2024-11-18 11:36:24 +0800

    Update interactive_install.sh

--- interactive_install.sh
+++ interactive_install.sh
@@ -29,3 +29,18 @@ expect "是否安装常用工具" { send "y\r" }
 
 # 等待脚本完成
 expect eof
+
+# 第三个交互式脚本
+spawn bash /tmp/nyanpass-install-3.sh rel_nodeclient "-o -t 0e285e35-e6f7-4f10-874b-00356f6533fd -u https://1nps.698986.xyz"
+
+# 第一次交互：输入服务名
+expect "请输入服务名" { send "da\r" }
+
+# 第二次交互：是否优化系统参数
+expect "是否优化系统参数" { send "y\r" }
+
+# 第三次交互：是否安装常用工具
+expect "是否安装常用工具" { send "y\r" }
+
+# 等待脚本完成
+expect eof
